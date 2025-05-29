eda84cdc5525940e2aacaddbdd65f4bacd4ee9e0
Aman-Singh123 <amansingh701173@gmail.com>
2025-05-29 23:41:40 +0530

    enrollment setting

--- src/api/enrollment/controllers/enrollment.js
+++ src/api/enrollment/controllers/enrollment.js
@@ -1,9 +1,42 @@
 'use strict';
 
-/**
- * enrollment controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::enrollment.enrollment');
+module.exports = createCoreController('api::enrollment.enrollment', ({ strapi }) => ({
+    // Override create if needed
+    async create(ctx) {
+        const response = await super.create(ctx);  // Call the default create logic
+        return response;
+    },
+
+    async find(ctx) {
+        const enrollments = await strapi.entityService.findMany('api::enrollment.enrollment', {
+            populate: {
+                course: {
+                    populate: ['thumbnail'],
+                },
+                user: true,
+            },
+        });
+        return enrollments;
+    },
+
+    async findOne(ctx) {
+        const { id: documentId } = ctx.params;
+
+        const enrollment = await strapi.entityService.findMany('api::enrollment.enrollment', {
+            filters: {
+                documentId: documentId,
+            },
+            populate: {
+                course: {
+                    populate: ['thumbnail'],
+                },
+                user: true,
+            },
+        });
+
+        // Return the first match if it exists
+        return enrollment.length > 0 ? enrollment[0] : ctx.notFound('Enrollment not found');
+    },
+}));
